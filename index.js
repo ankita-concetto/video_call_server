dfdc1eba4e63b85addad119593e6951a25be4cfe
nency-concetto <nensi.prajapati@concettolabs.com>
2022-05-30 19:44:17 +0530

    socketio config

--- index.js
+++ index.js
@@ -4,23 +4,15 @@ const io = require('socket.io')(server)
 io.on('connection', function (client) {
 
   console.log('client connect...', client.id);
-
-  client.on('typing', function name(data) {
-    console.log(data);
-    io.emit('typing', data)
-  })
+  //console.log('client connect --->...', client.request.ha);
 
   client.on('message', function name(data) {
     console.log(data);
     io.emit('message', data)
   })
 
-  client.on('location', function name(data) {
-    console.log(data);
-    io.emit('location', data);
-  })
-
   client.on('connect', function () {
+    console.log('connection established');
   })
 
   client.on('disconnect', function () {
@@ -39,3 +31,29 @@ server.listen(server_port, function (err) {
   if (err) throw err
   console.log('Listening on port %d', server_port);
 });
+
+function manageIncomingData(message, connection, request) {
+  let data
+  //accepting only JSON messages
+  try {
+      data = JSON.parse(message);
+      console.log(data);
+  } catch (e) {
+      console.log("Invalid JSON");
+      return;
+  }
+  switch (data.type) {
+      case constants.addUser:
+    var userId = data.message[constants.userId]
+          users[userId] = connection
+          request.name = userId;
+          sendTo(connection,{'type' : constants.addUser,'data' : {'result' : 'success'}})
+          break;
+      case constants.videoCall:
+    videoCall.manage(data.message, connection, request,users)
+          break;
+      case constants.audioCall:
+          audioCall.manage(data.message, connection, request,users)
+          break;
+  }
+}
